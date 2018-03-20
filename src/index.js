be2455d5591c7ffc9605605be61de8778d086baa
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-20 15:32:15 -0400

    ✨ require valid ego token

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import socketIO from 'socket.io';
 import { Server } from 'http';
 import { rainbow } from 'chalk-animation';
 import Arranger from '@arranger/server';
-
+import egoToken from 'ego-token-middleware';
 import shortUrlStatic from './shortUrlStatic';
 
 const port = process.env.PORT || 5050;
@@ -12,6 +12,8 @@ const http = Server(app);
 const io = socketIO(http);
 
 app.get('/s/:shortUrl', shortUrlStatic);
+
+app.use(egoToken({ required: true }));
 
 Arranger({ io }).then(router => {
   app.use(router);
