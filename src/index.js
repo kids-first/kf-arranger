4c980e89a7e1f0ab5798c70662bd8eed6286be81
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-20 11:37:37 -0400

    ✨ use exported router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import createServer from "@arranger/server";
-import shortUrlStatic from "./shortUrlStatic";
+import express from 'express';
+import socketIO from 'socket.io';
+import { Server } from 'http';
+import { rainbow } from 'chalk-animation';
+import Arranger from '@arranger/server';
 
-let server = createServer({
-  esHost: process.env.ES_HOST,
+import shortUrlStatic from './shortUrlStatic';
+
+const port = process.env.PORT || 5050;
+const app = express();
+const http = Server(app);
+const io = socketIO(http);
+
+app.get('/s/:shortUrl', shortUrlStatic);
+
+Arranger({ io }).then(router => {
+  app.use(router);
+  http.listen(port, async () => {
+    rainbow(`⚡️ Listening on port ${port} ⚡️`);
+  });
 });
-
-server.app.get("/s/:shortUrl", shortUrlStatic);
-
-server.listen(process.env.PORT || 5050);
