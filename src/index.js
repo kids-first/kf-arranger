7f5229ba6a0cab4426d3c291165e348a41621d08
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2018-03-29 14:21:45 -0400

    ✨ use ego-token-middleware (#7)
    * ✨ use exported router
    
    * ✨ require valid ego token
    
    * update to 0.0.3
    
    * update cors
    
    * don't require yet
    
    * update arranger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import createServer from "@arranger/server";
-import shortUrlStatic from "./shortUrlStatic";
+import 'babel-polyfill';
+import express from 'express';
+import socketIO from 'socket.io';
+import { Server } from 'http';
+import { rainbow } from 'chalk-animation';
+import Arranger from '@arranger/server';
+import egoToken from 'ego-token-middleware';
+import shortUrlStatic from './shortUrlStatic';
+import cors from 'cors';
 
-let server = createServer({
-  esHost: process.env.ES_HOST,
+const port = process.env.PORT || 5050;
+const app = express();
+const http = Server(app);
+const io = socketIO(http);
+app.use(cors());
+app.get('/s/:shortUrl', shortUrlStatic);
+
+app.use(egoToken({ required: false }));
+
+Arranger({ io }).then(router => {
+  app.use(router);
+  http.listen(port, async () => {
+    rainbow(`⚡️ Listening on port ${port} ⚡️`);
+  });
 });
-
-server.app.get("/s/:shortUrl", shortUrlStatic);
-
-server.listen(process.env.PORT || 5050);
