4dda10b221de4e45fbafeb482f88f5fa7f375ddc
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-12 14:14:35 -0400

    ✨ added a statistics endpoint that talks to ES directly (#43)
    * :sparkles: added a statistics endpoint that talks to ES directly
    
    * :bug: oops missed participants
    
    * :fire: remove unused var

--- src/env.js
+++ src/env.js
@@ -5,3 +5,6 @@ export const egoURL = process.env.EGO_API;
 export const projectId = process.env.PROJECT_ID;
 
 export const esHost = process.env.ES_HOST;
+
+export const esFileIndex = process.env.ES_FILE_INDEX;
+export const esFileType = process.env.ES_FILE_TYPE;
