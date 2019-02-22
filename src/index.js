a05277baf5219d84302c84da38c0e292af66cf4d
Minh Ha <hlminh2000@gmail.com>
2019-02-22 14:56:40 -0500

    imports getProject from arranger for later use
    * updates arranger
    
    * package.lock
    
    * updates arranger and imports getProject

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "babel-polyfill";
 import express from "express";
 import socketIO from "socket.io";
 import { Server } from "http";
-import Arranger from "@arranger/server";
+import Arranger, { getProject } from "@arranger/server";
 import egoTokenMiddleware from "ego-token-middleware";
 import cors from "cors";
 import bodyParser from "body-parser";
