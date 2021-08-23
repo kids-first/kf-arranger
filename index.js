9cdb200b3b6c636314f8dcfde3fd4bc7ee30b656
Céline Pelletier <celinepelletier@CRHSJ106784.local>
2021-08-23 08:32:53 -0400

    SKFP-82: Revert workaround related to missing .babelrc

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 #!/usr/bin/env node
 require('dotenv').config();
-// const fs = require('fs');
-// const babelCfgFile = fs.readFileSync('.babelrc', 'utf-8');
-const babelCfg = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: '11'
-        }
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-object-rest-spread'
-  ]
-};
-// try {
-//   babelCfg = JSON.parse(babelCfgFile);
-// } catch (err) {
-//   console.warn('Error parsing .babelrc file, ignoring');
-// }
+const fs = require('fs');
+const babelCfgFile = fs.readFileSync('.babelrc', 'utf-8');
+try {
+  babelCfg = JSON.parse(babelCfgFile);
+} catch (err) {
+  console.warn('Error parsing .babelrc file, ignoring');
+}
 require('@babel/register')(babelCfg);
 
 // Necessary to polyfill Babel (replaces @babel/polyfill since 7.4.0)
